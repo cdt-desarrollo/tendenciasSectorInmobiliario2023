95f67d5fa8b9ffbeb4b8b6979fb2b89783d051b4
Jair Hernández <124084942+cdt-desarrollo@users.noreply.github.com>
2023-06-02 12:05:01 -0700

    Frist commit

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ function getValues() {
   }
   data.push(companyName, staffName, email, phone)
   console.log(data)
-  if(data.length == 69){
-    if(data[66] == "")
+  if(data.length == 21){
+    if(data[18] == "")
     {
       Swal.fire({
         icon: "info",
@@ -23,7 +23,7 @@ function getValues() {
         confirmButtonColor: "#3085d6"
       })
     }
-    else if(data[66] != "")
+    else if(data[18] != "")
     {
       Swal.fire({
         position: "center",
@@ -47,79 +47,31 @@ function getValues() {
 function sendData(array) {
     var data = JSON.stringify({
       q1: `${array[0]}`,
-      m1q2: `${array[1]}`,
-      m2q2: `${array[2]}`,
-      m3q2: `${array[3]}`,
-      m4q2: `${array[4]}`,
-      m1q3: `${array[5]}`,
-      m2q3: `${array[6]}`,
-      m3q3: `${array[7]}`,
-      m4q3: `${array[8]}`,
-      m1q4: `${array[9]}`,
-      m2q4: `${array[10]}`,
-      m3q4: `${array[11]}`,
-      m4q4: `${array[12]}`,
-      m1q5: `${array[13]}`,
-      m2q5: `${array[14]}`,
-      m3q5: `${array[15]}`,
-      m4q5: `${array[16]}`,
-      m1q6: `${array[17]}`,
-      m2q6: `${array[18]}`,
-      m3q6: `${array[19]}`,
-      m4q6: `${array[20]}`,
-      m1q7: `${array[21]}`,
-      m2q7: `${array[22]}`,
-      m3q7: `${array[23]}`,
-      m4q7: `${array[24]}`,
-      m1q8: `${array[25]}`,
-      m2q8: `${array[26]}`,
-      m3q8: `${array[27]}`,
-      m4q8: `${array[28]}`,
-      m1q9: `${array[29]}`,
-      m2q9: `${array[30]}`,
-      m3q9: `${array[31]}`,
-      m4q9: `${array[32]}`,
-      m1q10: `${array[33]}`,
-      m2q10: `${array[34]}`,
-      m3q10: `${array[35]}`,
-      m4q10: `${array[36]}`,
-      m1q11: `${array[37]}`,
-      m2q11: `${array[38]}`,
-      m3q11: `${array[39]}`,
-      m4q11: `${array[40]}`,
-      m1q12: `${array[41]}`,
-      m2q12: `${array[42]}`,
-      m3q12: `${array[43]}`,
-      m4q12: `${array[44]}`,
-      m1q13: `${array[45]}`,
-      m2q13: `${array[46]}`,
-      m3q13: `${array[47]}`,
-      m4q13: `${array[48]}`,
-      m1q14: `${array[49]}`,
-      m2q14: `${array[50]}`,
-      m3q14: `${array[51]}`,
-      m4q14: `${array[52]}`,
-      m1q15: `${array[53]}`,
-      m2q15: `${array[54]}`,
-      m3q15: `${array[55]}`,
-      m4q15: `${array[56]}`,
-      m1q16: `${array[57]}`,
-      m2q16: `${array[58]}`,
-      m3q16: `${array[59]}`,
-      m4q16: `${array[60]}`,
-      m1q17: `${array[61]}`,
-      m2q17: `${array[62]}`,
-      m3q17: `${array[63]}`,
-      m4q17: `${array[64]}`,
-      name: `${array[65]}`,
-      company: `${array[66]}`,
-      email: `${array[67]}`,
-      phone: `${array[68]}`
+      q2: `${array[1]}`,
+      q3: `${array[2]}`,
+      q4: `${array[3]}`,
+      q5: `${array[4]}`,
+      q6: `${array[5]}`,
+      q7: `${array[6]}`,
+      q8: `${array[7]}`,
+      q9: `${array[8]}`,
+      q10: `${array[9]}`,
+      q11: `${array[10]}`,
+      q12: `${array[11]}`,
+      q13: `${array[12]}`,
+      q14: `${array[13]}`,
+      q15: `${array[14]}`,
+      q16: `${array[15]}`,
+      q17: `${array[16]}`,
+      name: `${array[17]}`,
+      company: `${array[18]}`,
+      email: `${array[19]}`,
+      phone: `${array[20]}`
     });
     // sheet.best
     var config = {
       method: "post",
-      url: "https://sheet.best/api/sheets/32aacf91-902e-4e88-8d70-e596b2f41ceb/tabs/enemay2023",
+      url: "https://sheet.best/api/sheets/32aacf91-902e-4e88-8d70-e596b2f41ceb/tabs/may2023",
       mode: "cors",
       headers: {
         "Content-Type": "application/json",
@@ -183,35 +135,7 @@ function escribirCiudad(inputObject) {
         escribirCiudad(inputObject)
       }
       else if(ciudad != ""){
-        if (inputObject.id == "otroseneq11") {
-          document.getElementById("insertCityeneq11").innerHTML = "";
-          document.getElementById("otroseneq11").value = ciudad;
-          document.getElementById(
-            "insertCityeneq11"
-          ).innerHTML += `<h6><mark>${ciudad}</mark></h6>`;
-        }
-        else if (inputObject.id == "otrosfebq11") {
-          document.getElementById("insertCityfebq11").innerHTML = "";
-          document.getElementById("otrosfebq11").value = ciudad;
-          document.getElementById(
-            "insertCityfebq11"
-          ).innerHTML += `<h6><mark>${ciudad}</mark></h6>`;
-        }
-        else if (inputObject.id == "otrosmarq11") {
-          document.getElementById("insertCitymarq11").innerHTML = "";
-          document.getElementById("otrosmarq11").value = ciudad;
-          document.getElementById(
-            "insertCitymarq11"
-          ).innerHTML += `<h6><mark>${ciudad}</mark></h6>`;
-        }
-        else if (inputObject.id == "otrosabrq11") {
-          document.getElementById("insertCityabrq11").innerHTML = "";
-          document.getElementById("otrosabrq11").value = ciudad;
-          document.getElementById(
-            "insertCityabrq11"
-          ).innerHTML += `<h6><mark>${ciudad}</mark></h6>`;
-        }
-        else if (inputObject.id == "otrosmayq11") {
+        if (inputObject.id == "otrosmayq11") {
           document.getElementById("insertCitymayq11").innerHTML = "";
           document.getElementById("otrosmayq11").value = ciudad;
           document.getElementById(
@@ -232,34 +156,6 @@ function escribirUso(inputObject) {
         escribirUso(inputObject)
       }
       else if(uso != ""){
-        if(inputObject.id == "otroseneq15"){
-          document.getElementById("insertUseeneq15").innerHTML = "";
-          document.getElementById("otroseneq15").value = uso;
-          document.getElementById(
-            "insertUseeneq15"
-          ).innerHTML += `<h6><mark>${uso}</mark></h6>`;
-        }
-        if(inputObject.id == "otrosfebq15"){
-          document.getElementById("insertUsefebq15").innerHTML = "";
-          document.getElementById("otrosfebq15").value = uso;
-          document.getElementById(
-            "insertUsefebq15"
-          ).innerHTML += `<h6><mark>${uso}</mark></h6>`;
-        }
-        if(inputObject.id == "otrosmarq15"){
-          document.getElementById("insertUsemarq15").innerHTML = "";
-          document.getElementById("otrosmarq15").value = uso;
-          document.getElementById(
-            "insertUsemarq15"
-          ).innerHTML += `<h6><mark>${uso}</mark></h6>`;
-        }
-        if(inputObject.id == "otrosabrq15"){
-          document.getElementById("insertUseabrq15").innerHTML = "";
-          document.getElementById("otrosabrq15").value = uso;
-          document.getElementById(
-            "insertUseabrq15"
-          ).innerHTML += `<h6><mark>${uso}</mark></h6>`;
-        }
         if(inputObject.id == "otrosmayq15"){
           document.getElementById("insertUsemayq15").innerHTML = "";
           document.getElementById("otrosmayq15").value = uso;
